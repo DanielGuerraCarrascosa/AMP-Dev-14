dd6b25c75d75b1a38ea92f5e5df2e3467273ef18
DanielGuerraCarrascosa <d.guerra@ampsoftware.com>
2021-05-26 07:54:33 +0000

    [ADD] Basic Module

---
+++ odoo_academy/__manifest__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Odoo Academy',
+    
+    'sumary': """ Academy app para hacer pruebas""",
+    
+    'description': """ Modulo academia para realizar pruebas""",
+    
+    'author': 'Dani AMP Software',
+    
+    'Website': 'https://www.odoo.com',
+    
+    'category': 'Training',
+    
+    'version': '0.1',
+    
+    'depends': ['base'],
+    
+    'data': [
+        
+    ],
+    
+    'demo': [
+        
+    ],
+}
