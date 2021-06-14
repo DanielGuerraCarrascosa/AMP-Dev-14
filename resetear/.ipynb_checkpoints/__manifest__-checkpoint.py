fa7d90b59aae170c07fb217384f803ab4847adb1
DanielGuerraCarrascosa <d.guerra@ampsoftware.com>
2021-06-14 12:16:20 +0000

    [ADD] modulo resetear

---
+++ resetear/.ipynb_checkpoints/__manifest__-checkpoint.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Resetear Contraseña',
+    
+    'sumary': """ Resetea contraseña - email por url""",
+    
+    'description': """ Resetea contraseña enviando email como variable por url """,
+    
+    'author': 'Dani AMP Software',
+    
+    'Website': 'https://www.odoo.com',
+    
+    'category': 'Training',
+    
+    'version': '0.1',
+    
+    'depends': ['website'],
+    
+    'data': [
+        'views/resetear_views.xml',
+    ],
+    
+    'demo': [
+    ],
+}
